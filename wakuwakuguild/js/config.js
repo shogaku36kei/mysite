04d616845cd87fdb1c9b65722e21bc8b3d8e46ef
Shogaku36kei <makeukumikyoku@gmail.com>
2026-02-26 13:15:28 +0900

    Update GAS Web App URL in config.js

--- wakuwakuguild/js/config.js
+++ wakuwakuguild/js/config.js
@@ -1,10 +1,10 @@
-// GAS Web App URL（デプロイ後に設定）
-const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/1CcvVi2Py5A2LHPWvKy9mUrwLLBETGZE_AoXE6qlhvfo/exec';
+// GAS Web App URL（新しくデプロイしたURLに変更）
+const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbw6IRzmac7GOGiP5cYkwud4VldS_oApUbQ4S6C54O87DIDtVwVkolkrQtGC45lf2gtzlA/exec';
 
 // その他の設定
 const CONFIG = {
   siteName: 'わくわくギルド',
   adminEmail: 'guild@city.soja.okayama.jp',
-  maxQuestReward: 1000000, // 最大報酬額
-  taxRate: 0.1021 // 源泉徴収税率
+  maxQuestReward: 1000000,
+  taxRate: 0.1021
 };
