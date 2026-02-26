6015e0fff6aa2b1927ba35bf51b8fc5fa0dea6cd
Shogaku36kei <makeukumikyoku@gmail.com>
2026-02-26 11:01:37 +0900

    Add configuration settings for GAS Web App

---
+++ wakuwakuguild/js/config.js
@@ -0,0 +1,10 @@
+// GAS Web App URL（デプロイ後に設定）
+const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/1CcvVi2Py5A2LHPWvKy9mUrwLLBETGZE_AoXE6qlhvfo/exec';
+
+// その他の設定
+const CONFIG = {
+  siteName: 'わくわくギルド',
+  adminEmail: 'guild@city.soja.okayama.jp',
+  maxQuestReward: 1000000, // 最大報酬額
+  taxRate: 0.1021 // 源泉徴収税率
+};
